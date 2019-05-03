515e826db740e932e56b6c29bef914535b4eeb02
Patrick Stahl <p.stahl@shopware.com>
2019-05-03 08:40:06 +0200

    Fix outdated code for EntityDefinitions

--- src/Custom/CustomEntity.php
+++ src/Custom/CustomEntity.php
@@ -14,16 +14,6 @@ class CustomEntity extends Entity
      */
     protected $technicalName;
 
-    /**
-     * @var \DateTimeInterface
-     */
-    protected $createdAt;
-
-    /**
-     * @var \DateTimeInterface|null
-     */
-    protected $updatedAt;
-
     public function getTechnicalName(): string
     {
         return $this->technicalName;
@@ -32,25 +22,5 @@ class CustomEntity extends Entity
     public function setTechnicalName(string $technicalName): void
     {
         $this->technicalName = $technicalName;
-    }
-
-    public function getCreatedAt(): \DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): void
-    {
-        $this->createdAt = $createdAt;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): void
-    {
-        $this->updatedAt = $updatedAt;
     }
 }
--- src/Custom/CustomEntityDefinition.php
+++ src/Custom/CustomEntityDefinition.php
@@ -3,38 +3,34 @@
 namespace Swag\CustomEntity\Custom;
 
 use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\CreatedAtField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\UpdatedAtField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
 
 class CustomEntityDefinition extends EntityDefinition
 {
-    public static function getEntityName(): string
+    public function getEntityName(): string
     {
         return 'custom_entity';
     }
 
-    protected static function defineFields(): FieldCollection
-    {
-        return new FieldCollection([
-            (new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
-            new StringField('technical_name', 'technicalName'),
-            new CreatedAtField(),
-            new UpdatedAtField(),
-        ]);
-    }
-
-    public static function getCollectionClass(): string
+    public function getCollectionClass(): string
     {
         return CustomEntityCollection::class;
     }
 
-    public static function getEntityClass(): string
+    public function getEntityClass(): string
     {
         return CustomEntity::class;
+    }
+
+    protected function defineFields(): FieldCollection
+    {
+        return new FieldCollection([
+            (new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
+            new StringField('technical_name', 'technicalName'),
+        ]);
     }
 }
